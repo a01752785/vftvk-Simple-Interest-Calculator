e31dbb466c53ffe7dfc957afdbfc138a769888b9
a01752785 <a01752785@itesm.mx>
2022-03-10 20:53:22 -0600

    Adds input validation and styles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function computeInterest(amount, interestRate, years)
 {
-    var result = amount * Math.pow((1 + interestRate), years);
+    var result = amount * years * interestRate;
     return result;
 }
 
@@ -9,10 +9,17 @@ function generateOutput() {
     var interestRate = Number(document.getElementById("rate").value) / 100;
     var years = Number(document.getElementById("years").value);
     var result = computeInterest(amount, interestRate, years);
+    var futureYear = new Date().getFullYear() + parseInt(years);
+
+    if (amount <= 0) {
+        alert("Enter a positive number");
+        return;
+    }
+
     var outputStr = "If you deposit " + "<span class=\"numeric-result\">" + amount + "<\/span> <br\/>"
-        + "at an interest rate of " + "<span class=\"numeric-result\">" + interestRate + "%<\/span>. <br\/>"
+        + "at an interest rate of " + "<span class=\"numeric-result\">" + (interestRate * 100) + "%<\/span>. <br\/>"
         + "You will receive an amount of " + "<span class=\"numeric-result\">" + result + "<\/span>, <br\/>"
-        + "in the year " + "<span class=\"numeric-result\">" + (2020 + years) + "<\/span> <br\/>";
+        + "in the year " + "<span class=\"numeric-result\">" + futureYear + "<\/span> <br\/>";
     document.getElementById("result").innerHTML = outputStr;
 }
 
